4872555f9c52ec6c21bcbc9628ccd62baee667a8
ptr5000 <35306800+ptr5000@users.noreply.github.com>
2018-07-25 11:43:12 +0300

    fix

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 class Item:
     def __init__(self, name):
         self.name = name
+
 
 nimi = "Essi Esimerkki"
 
@@ -21,9 +23,7 @@ def hello():
 
 @app.route("/demo")
 def content():
-    return render_template("demo.html", nimi=nimi, lista=lista, esineet=esineet)
+    return render_template("index.html", nimi=nimi, lista=lista, esineet=esineet)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
