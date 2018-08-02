5c2215fa36c38eccb48f7db543b56ed9b865d489
ptr5000 <35306800+ptr5000@users.noreply.github.com>
2018-08-02 14:24:18 +0300

    minor fixes and improvements to login and reg

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,41 +6,37 @@ from app.auth.forms import LoginForm, RegistrationForm
 
 @app.route("/auth/login")
 def auth_login_form():
-    return _render_login_form(form=LoginForm(), 
-                              registration_form=RegistrationForm())
+    return _render_forms(form=LoginForm(), 
+                         registration_form=RegistrationForm())
 
 
 @app.route("/auth/login", methods=["POST"])
 def auth_handle_login():
     form = LoginForm(request.form)
 
-    user = User.query.filter_by(username=form.username.data, 
-                                password=form.password.data).first()
-
-    if not user:
-        return _render_login_form(form=form, 
-                                  registration_form=RegistrationForm(), 
-                                  error="Invalid credientals")
-
-    login_user(user)
-
+    if not _login_user(form):
+        return _render_forms(form=form, 
+                             registration_form=RegistrationForm(), 
+                             error="Invalid credentials")
+    
     return redirect(url_for("index"))
+
+
+@app.route("/auth/register", methods=["GET"])
+def auth_register():
+    return _render_forms(form=LoginForm(),
+                         registration_form=RegistrationForm(),
+                         active_tab="register")
 
 
 @app.route("/auth/register", methods=["POST"])
 def auth_handle_registration():
     form = RegistrationForm(request.form)
 
-    if form.validate():
-        print("Form validated")
-        user = User(form.username.data, form.password.data)
-        db.session().add(user)
-        db.session().commit()
-        login_user(user)
-    else:
-        print("Got an error")
-        return _render_login_form(form=LoginForm(),
-                                  registration_form=form)
+    if not _add_user_to_db(form):
+        return _render_forms(form=LoginForm(),
+                             registration_form=form,
+                             active_tab="register")
 
     return redirect(url_for("index"))
 
@@ -50,7 +46,34 @@ def auth_handle_logout():
     logout_user()
     return redirect(url_for("index"))
 
-def _render_login_form(form, registration_form, error=None):
+
+def _add_user_to_db(form):
+    if form.validate():
+        user = User(form.username.data, form.password.data)
+        db.session().add(user)
+        db.session().commit()
+        login_user(user)
+        return True
+
+    return False
+
+
+def _login_user(form):
+    user = User.query.filter_by(username=form.username.data, 
+                                password=form.password.data).first()
+
+    if user:
+        login_user(user)
+        return True
+
+    return False
+    
+
+
+def _render_forms(form, registration_form, active_tab="login", error=None):
     return render_template("auth/auth-forms.html", form=form,
                            reg_form=registration_form,
+                           active_tab=active_tab,
                            error=error)
+
+
