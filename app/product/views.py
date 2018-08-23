1f310c0b46dc984369c4ef9002a8ecf6bcc4e270
ptr5000 <35306800+ptr5000@users.noreply.github.com>
2018-08-23 17:55:39 +0300

    cleanup

--- app/product/views.py
+++ app/product/views.py
@@ -46,7 +46,7 @@ def product_edit_existing_form(id=None):
 @app.route('/product/<id>/delete')
 @login_required(Roles.ADMIN)
 def product_perform_delete(id=None):
-    model = _get_product_model_or_abort(id)
+    model = _get_product_or_abort(id)
     db.session().delete(model)
     db.session().commit()
 
@@ -56,17 +56,17 @@ def product_perform_delete(id=None):
 @app.route('/product/suppliers/<id>', methods=["GET"])
 @login_required(Roles.ADMIN)
 def product_suppliers(id=None):
-    model = _get_product_model_or_abort(id)
+    product = _get_product_or_abort(id)
 
     return render_template("product/suppliers-list.html", 
-                           product=model, 
-                           suppliers=model.get_all_suppliers())
+                           product=product, 
+                           suppliers=product.get_all_suppliers())
 
 
 @app.route('/product/<id>', methods=["POST"])
 @login_required(Roles.ADMIN)
 def product_perform_update(id=None):
-    model = _get_product_model_or_abort(id)
+    model = _get_product_or_abort(id)
     form = ProductForm(request.form, model)
 
     if validate_and_populate_form_model(form, model):
@@ -79,7 +79,7 @@ def _render_product_form(form):
     return render_template("product/product-form-standalone.html", form=form)
 
 
-def _get_product_model_or_abort(id):
+def _get_product_or_abort(id):
     model = Product.query.get(id)
 
     if not model:
@@ -88,6 +88,6 @@ def _get_product_model_or_abort(id):
     return model
 
 def _create_product_form(id):
-    model = _get_product_model_or_abort(id)
+    model = _get_product_or_abort(id)
     form = ProductForm(request.form, model)
     return form
--- app/subcontractor/models.py
+++ app/subcontractor/models.py
@@ -1,12 +1,14 @@
+from sqlalchemy.sql import text
 from app import db
 from app.models import BaseAddressModel
-from sqlalchemy.sql import text
+
 
 class Subcontractor(BaseAddressModel):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("account.id"), index=True, nullable=False)
     vat_code = db.Column(db.String(255), nullable=False)
     paypal_address = db.Column(db.String(255), nullable=False)
+
 
     @staticmethod
     def get_details(id):
@@ -16,11 +18,11 @@ class Subcontractor(BaseAddressModel):
                             "account.username, SUM(invoice.amount) "
                     "FROM subcontractor, invoice, account "
                     "WHERE subcontractor.id=:subcontractor_id "
-                        "AND invoice.subcontractor_id=subcontractor.id "
+                        "AND invoice.subcontractor_id = subcontractor.id "
                         "AND account.id = subcontractor.user_id "
                         "GROUP BY subcontractor.company_name, subcontractor.street, "
                                     "subcontractor.city, subcontractor.state,subcontractor.country, "
-                                    " subcontractor.zip_code,account.username").params(subcontractor_id=id)
+                                    "subcontractor.zip_code,account.username").params(subcontractor_id=id)
 
         res = db.engine.execute(stmt)
         row = res.fetchone()
